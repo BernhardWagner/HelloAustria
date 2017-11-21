1f5756d3054a52b605c20c7f1703c79ebca7a70a
melaniehumer <melaniehumer94@gmail.com>
2017-11-21 18:40:16 +0100

    Infobox

--- assets/js/mh-script.js
+++ assets/js/mh-script.js
@@ -1,17 +1,32 @@
-$(document).ready(function() {
-  $("#menucross").hide();
-  $("#menuitem").hide();
-  $("#menubutton").click(function() {
-    $("#menuitem").slideToggle("slow", function() {
-      $("#menubutton").hide();
-      $("#menucross").show();
-    });
+function openNav() {
+    document.getElementById("menuitem").style.width = "150px";
+}
+
+function closeNav() {
+    document.getElementById("menuitem").style.width = "0";
+}
+
+
+
+$( document ).ready(function() {
+
+  $( "#menucross" ).hide();
+
+  $( "#menubutton" ).click(function() {
+    $( "#menubutton" ).hide();
+    $( "#menucross" ).show();
   });
 
-  $("#menucross").click(function() {
-    $("#menuitem").slideToggle("slow", function() {
-      $("#menucross").hide();
-      $("#menubutton").show();
-    });
+  $( "#menucross" ).click(function() {
+    $( "#menucross" ).hide();
+    $( "#menubutton" ).show();
   });
+
+  $( ".info" ).click(function() {
+    console.log("jhkadhjk");
+     $( "div.infobox" ).toggleClass( "hiddeninfobox" );
+  });
+
+
+
 });
