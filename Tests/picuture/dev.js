a4891ef5b0552104abe28b37482ab1d4e312f80e
BernhardWagner <S1510238076@students.fh-hagenberg.at>
2017-11-24 13:48:03 +0100

    added a photo module to register a photo an click (only works once per place)

---
+++ Tests/picuture/dev.js
@@ -0,0 +1,59 @@
+"use strict";
+var bw = bw || {};
+
+
+bw.pictureTest = (function ($) {
+
+    var canvas, stage, exportRoot, idleSeconds = 10;
+
+    function init(canvas_) {
+        canvas = canvas_;
+
+        exportRoot = new lib.grossglockner();
+        stage = new createjs.Stage(canvas);
+        stage.addChild(exportRoot);
+        stage.update();
+
+        stage.mouseEnabled = true;
+        stage.enableMouseOver(24);
+
+        bw.idle.registerIdleHints(idleSeconds, stage);
+
+
+        bw.sceneChanger.registerSceneChanger(
+            [exportRoot.sc1],           //standard animaitons for scene "in", "action", "out"
+            stage,
+            exportRoot,
+            [[exportRoot.sc1.l1, exportRoot.sc1.l2, exportRoot.sc1.l3, exportRoot.sc1.l4]], //Scenes first array is first scene, second is second scene...
+            [[20,50,100, 200]], [], []); //secon last array for interaction objects (also seperated per subscene)
+
+
+        createjs.Ticker.setFPS(24);
+        createjs.Ticker.addEventListener("tick", tickHandler);
+
+        console.log(exportRoot.sc1);
+
+        bw.photo.registerPictureInteraction(exportRoot.sc1.l4, exportRoot.sc1.photo, null, exportRoot.sc1.photo.pic.close);
+
+        $(document).on("keypress", function (e) {
+            if(e.keyCode === 114) {
+
+            }
+            if(e.keyCode === 107) {
+            }
+        })
+    }
+
+    function tickHandler() {
+
+
+        stage.update();
+    }
+
+
+    return {
+        init: init,
+    }
+
+
+}($));
---
+++ assets/js/bw-canvas-modules/bw-photo.js
@@ -0,0 +1,94 @@
+"use strict";
+
+var bw = bw || {};
+
+
+bw.photo = (function ($) {
+
+    var triggerElement, pictureObject, pictureSound, closeButton, active;
+
+    /* TODO only one picture interaction per place? */
+    function registerPictureInteraction(triggerElement_, pictureObject_, sound_, closeButton_) {
+        triggerElement = triggerElement_;
+        pictureObject = pictureObject_;
+        setPictureSound(sound_);
+        closeButton = closeButton_;
+        closeButton.cursor = "pointer";
+        triggerElement.cursor = "pointer";
+        active = false;
+
+        triggerElement.addEventListener('click', makeAPhoto);
+        closeButton.addEventListener('click', closeImage);
+        closeButton.addEventListener('mouseover', hoverCloseButton);
+        closeButton.addEventListener('mouseout', nohoverCloseButton);
+
+    }
+
+    function unregisterPictureIntearction() {
+        triggerElement.removeEventListener('click', makeAPhoto);
+        closeButton.removeEventListener('click', closeImage);
+        closeButton.removeEventListener('mouseover', hoverCloseButton);
+        closeButton.removeEventListener('mouseout', nohoverCloseButton);
+
+        pictureSound = null;
+        triggerElement = null;
+        pictureObject = null;
+        pictureSound = null;
+        active = false;
+        closeButton = null;
+    }
+
+
+    function makeAPhoto() {
+        if(!active) {
+            active = true;
+
+            if(pictureSound){
+                pictureSound.play();
+            }
+
+            pictureObject.gotoAndPlay('in');
+        }
+    }
+
+    function closeImage() {
+        pictureObject.gotoAndPlay('out');
+        active = false;
+    }
+    
+    function setPictureSound(sound_) {
+        if(sound_){
+            if(typeof sound_ === 'object'){
+                pictureSound = sound_;
+            }
+
+            else if(typeof sound_ === 'string') {
+                pictureSound = createjs.Sound.createInstance(sound_);
+            }
+        }
+    }
+
+    function hoverCloseButton() {
+        console.log("hoverIn");
+        var newScaleX = closeButton.scaleX * 1.1,
+            newScaleY = closeButton.scaleY * 1.1;
+
+        createjs.Tween.get(closeButton, {override: true}).to({scaleX: newScaleX, scaleY: newScaleY}, 200, createjs.Ease.cubicIn);
+    }
+
+    function nohoverCloseButton() {
+        console.log("hoverOut");
+        var newScaleX = closeButton.scaleX / 1.1,
+            newScaleY = closeButton.scaleY / 1.1;
+
+        createjs.Tween.get(closeButton, {override: true}).to({scaleX: newScaleX, scaleY: newScaleY}, 200, createjs.Ease.cubicIn);
+    }
+
+
+    return {
+        registerPictureInteraction: registerPictureInteraction,
+        unregisterPictureIntearction: unregisterPictureIntearction
+    }
+
+
+}($));
