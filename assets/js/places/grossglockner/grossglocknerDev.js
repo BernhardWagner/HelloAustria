0f112996e4310925554d02dae6004508e573a23d
BernhardWagner <S1510238076@students.fh-hagenberg.at>
2017-11-26 00:07:09 +0100

    bug fixes

--- assets/js/places/grossglockner/grossglocknerDev.js
+++ assets/js/places/grossglockner/grossglocknerDev.js
@@ -41,6 +41,44 @@ bw.grossglockner = (function ($) {
 
     }
 
+
+    //TODO: is needed?
+    function makeResponsive(isResp, respDim, isScale, scaleType) {
+        var lastW, lastH, lastS=1;
+        window.addEventListener('resize', resizeCanvas);
+        resizeCanvas();
+        function resizeCanvas() {
+            var w = lib.properties.width, h = lib.properties.height;
+            var iw = window.innerWidth, ih=window.innerHeight;
+            var pRatio = window.devicePixelRatio || 1, xRatio=iw/w, yRatio=ih/h, sRatio=1;
+            if(isResp) {
+                if((respDim=='width'&&lastW==iw) || (respDim=='height'&&lastH==ih)) {
+                    sRatio = lastS;
+                }
+                else if(!isScale) {
+                    if(iw<w || ih<h)
+                        sRatio = Math.min(xRatio, yRatio);
+                }
+                else if(scaleType==1) {
+                    sRatio = Math.min(xRatio, yRatio);
+                }
+                else if(scaleType==2) {
+                    sRatio = Math.max(xRatio, yRatio);
+                }
+            }
+            canvas.width = w*pRatio*sRatio;
+            canvas.height = h*pRatio*sRatio;
+            canvas.style.width = dom_overlay_container.style.width = anim_container.style.width =  w*sRatio+'px';
+            canvas.style.height = anim_container.style.height = dom_overlay_container.style.height = h*sRatio+'px';
+            stage.scaleX = pRatio*sRatio;
+            stage.scaleY = pRatio*sRatio;
+            lastW = iw; lastH = ih; lastS = sRatio;
+            stage.tickOnUpdate = false;
+            stage.update();
+            stage.tickOnUpdate = true;
+        }
+    }
+
     function unregister() {
             bw.sceneChanger.unregisterSceneChanger();
     }
